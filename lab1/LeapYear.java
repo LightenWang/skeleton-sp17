0ba6aafafaf19fe9f7e8dfc34eb533c8691bbab9
LightenWang <Lightenwang@gmail.com>
2018-11-21 21:35:39 +0800

    add the isLeapYear() method, completed LeapYear lab.

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,21 @@ public class LeapYear {
         }
     }
 
+    /** determine a specified year is a leap year or not
+     * @param  year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 4 != 0) {
+            return false;
+        } else if (year % 100 != 0) {
+            return true;
+        } else if (year % 400 != 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
